faa8149950ffb3d10dfb8a8ef34458ce25b2aa44
Dyego Costa <dyego@dyegocosta.com>
2022-05-27 13:59:36 -0300

    Only add Authorization header if auth token is provided

--- runTests.js
+++ runTests.js
@@ -5,13 +5,16 @@ import { Httpx } from 'https://jslib.k6.io/httpx/0.0.3/index.js';
 const session = new Httpx({
   baseURL: __ENV.BASE_URL,
   headers: {
-    'Authorization': `Bearer ${__ENV.AUTH_TOKEN}`,
     accept: 'application/json, text/javascript, */*; q=0.01',
     'Accept-Encoding': 'gzip, deflate, br',
     'Content-Type': 'application/json',
   },
   timeout: 20000, // 20s timeout.
 });
+
+if (__ENV.AUTH_TOKEN) {
+  session.addHeader('Authorization', `Bearer ${__ENV.AUTH_TOKEN}`)
+}
 
 /* Options
 Global options
@@ -30,7 +33,7 @@ export let options = {
   ],
   thresholds: {
     // We want the 95th percentile of all HTTP request durations to be less than 500ms
-    http_req_duration: ['p(95)<500'],
+    http_req_duration: ['p(95)<250'],
     // Thresholds based on the custom metric we defined and use to track application failures
     check_failure_rate: [
       // Global failure rate should be less than 1%
